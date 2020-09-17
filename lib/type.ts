65d413f73a5c32411498273fbe1fc402a40de538
bsbc <bsbc.app@gmail.com>
2020-09-17 14:13:39 +0900

    1. typescript target을 ES2017로 변경 2. 유니코드 변환 기능 추가 3. (2) 함수 테스트 코드 추가

--- lib/type.ts
+++ lib/type.ts
@@ -13,4 +13,46 @@ export class Type {
 
     return ret;
   }
+
+  unescapeUnicode(data: any): any {
+    function doUnescape(str: any): any {
+      let val = str;
+
+      if (typeof str === 'string') {
+        val = str.replace(/\u200B/g, '').trim();
+      }
+
+      return val;
+    }
+
+    function loopUnescape(child: any): any {
+      let ret: any = null;
+
+      switch(typeof child) {
+        case 'object': {
+          if (child != null) {
+            ret = {};
+
+            Object.entries(child).forEach(([key, value]) => {
+              ret[key] = loopUnescape(value);
+            });
+          } else {
+            ret = null;
+          }
+
+          break;
+        }
+        case 'string':
+          ret = doUnescape(child);
+          break;
+        default:
+          ret = child;
+          break;
+      }
+
+      return ret;
+    }
+
+    return loopUnescape(data);
+  }
 }
--- test/type.js
+++ test/type.js
@@ -24,3 +24,65 @@ describe('type.isJSON test', () => {
     expect(util.type.isJSON(str)).to.equal(false);
   });
 });
+
+describe('type.unescapeUnicode test', () => {
+  it('Single ascii string', () => {
+    const input = 'abcdefg';
+    const comp = 'abcdefg';
+    const output = util.type.unescapeUnicode(input);
+
+    expect(comp === output).to.equal(true);
+  });
+
+  it('Single unicode string', () => {
+    const input = '\uD14C\uC2A4\uD2B8';
+    const comp = '테스트';
+    const output = util.type.unescapeUnicode(input);
+
+    expect(comp === output).to.equal(true);
+  });
+
+  it('Object type', () => {
+    const input = {
+      a: 'abc123!@#',
+      b: '\uD14C\uC2A4\uD2B8',
+      c: 123,
+      d: null,
+      e: {
+        a: 'abc123!@#',
+        b: '\uD14C\uC2A4\uD2B8',
+        c: 123,
+        d: null,
+        e: {
+          a: 'abc123!@#',
+          b: '\uD14C\uC2A4\uD2B8',
+          c: 123,
+          d: null,
+        },
+      },
+    };
+
+    const comp = {
+      a: 'abc123!@#',
+      b: '\uD14C\uC2A4\uD2B8',
+      c: 123,
+      d: null,
+      e: {
+        a: 'abc123!@#',
+        b: '\uD14C\uC2A4\uD2B8',
+        c: 123,
+        d: null,
+        e: {
+          a: 'abc123!@#',
+          b: '\uD14C\uC2A4\uD2B8',
+          c: 123,
+          d: null,
+        },
+      },
+    };
+
+    const output = util.type.unescapeUnicode(input);
+
+    expect(JSON.stringify(comp) === JSON.stringify(output)).to.equal(true);
+  });
+});
